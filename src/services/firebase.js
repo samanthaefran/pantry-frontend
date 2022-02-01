9a8c6f007c52aecdbf18a3d657f186e262e11144
samantha frangi <sefrangi13@gmail.com>
2022-02-01 12:37:49 -0500

    successfully adds login feature

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAmRh1WBQ7oDkpp6Q7i3cvmfwaMo3pus2A",
