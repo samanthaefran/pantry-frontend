868377b58d4e06ae88cbf0fd883d244656083e87
samantha frangi <sefrangi13@gmail.com>
2022-02-01 12:38:36 -0500

    fixes typo in home file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ function Home(props) {
   return (
     <div className="main">
       <button onClick={login}>Login</button>
-      <button coClick={logout}>Logout</button>
+      <button onClick={logout}>Logout</button>
       <article>
         Pantry is the go-to place to manage your food items and create intentional shopping lists to curb food waste in your home.
       </article></div>
